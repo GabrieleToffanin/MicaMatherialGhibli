4d512a12559d542ec3a6f6963cfc2bc76f521ac2
Gabriele <GabrieleToffanin@outlook.it>
2022-04-03 18:26:10 +0200

    Settings DI Implementation for Chaching (?)

---
+++ Model/SettingsService.cs
@@ -0,0 +1,28 @@
+﻿using MicaMatherialGhibli.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+
+namespace MicaMatherialGhibli.Model
+{
+    public class SettingsService : ISettingsService
+    {
+        private readonly IPropertySet SettingsStorage = ApplicationData.Current.LocalSettings.Values;
+
+        public void SetValue<T>(string key, T value)
+        {
+            if(!SettingsStorage.ContainsKey(key)) SettingsStorage.Add(key, value);
+            else SettingsStorage[key] = value;
+        }
+
+        public T GetValue<T>(string key)
+        {
+            if(SettingsStorage.TryGetValue(key, out object value)) return (T)value;
+            return default;
+        }
+    }
+}
---
+++ Services/ISettingsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicaMatherialGhibli.Services
+{
+    public interface ISettingsService
+    {
+        void SetValue<T>(string key, T value);
+        [Pure]
+        T GetValue<T>(string key);
+    }
+}
